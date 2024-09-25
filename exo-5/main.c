19633d684d4735a5bd9922fb228271b90da77c25
NeethDseven <labidi.neeth@gmail.com>
2024-09-25 23:30:36 +0200

    exo5

---
+++ exo-5/main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+  double a = 7.0;
+  double b = 12.0;
+  double c = (a + b) / 2;
+
+printf("La moyenne de 12 et 7 est égale à %.2f\n",c);
+	return 0;
+}
